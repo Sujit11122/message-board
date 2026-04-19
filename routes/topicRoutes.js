691654aeba3bf4d03f092ee1fa4aaa87577a056a
Sagar Bastola <sagar.bastola@selu.edu>
2026-04-19 10:20:42 -0500

    Add topic routes

---
+++ routes/topicRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const topicController = require('../controllers/topicController');
+const messageController = require('../controllers/messageController');
+const statsController = require('../controllers/statsController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.get('/dashboard', authMiddleware, topicController.getDashboard);
+router.get('/topics', authMiddleware, topicController.getAllTopics);
+router.post('/topics', authMiddleware, topicController.createTopic);
+router.get('/topics/:id', authMiddleware, topicController.getTopicDetail);
+router.post('/topics/:id/subscribe', authMiddleware, topicController.subscribeTopic);
+router.post('/topics/:id/unsubscribe', authMiddleware, topicController.unsubscribeTopic);
+router.post('/topics/:topicId/messages', authMiddleware, messageController.postMessage);
+router.get('/stats', authMiddleware, statsController.getStats);
+
+module.exports = router;
